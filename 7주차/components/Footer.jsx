92fab4c0fe6f191d41f21e621a0f389fa51115f7
jhyeon1027 <150174766+jhyeon1027@users.noreply.github.com>
2025-05-18 17:40:04 +0900

    -

---
+++ 7주차/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+export const Footer = () => {
+    return (
+        <footer>
+        <h2>
+            This is footer
+        </h2>
+    </footer>)
+}
---
+++ 7주차/page/Home.jsx
@@ -0,0 +1,12 @@
+import {Header} from "../components/Header.jsx";
+import {Footer} from "../components/Footer.jsx";
+
+export const Home = () => {
+    return(
+        <div>
+            <Header/>
+
+            <Footer/>
+        </div>
+    )
+}
